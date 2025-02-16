c579f1c947f99197ed2cf83678a46e245015242e
Hanh <hanh425@gmail.com>
2025-02-16 11:52:17 +1000

    feat: use shadcn component toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,188 +1,221 @@
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import "./App.css";
-import { Button, Card, Checkbox, Label, Modal, Progress, Textarea, TextInput } from "flowbite-react";
 import { Channel, invoke } from "@tauri-apps/api/core";
 import { useState } from "react";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { save } from "@tauri-apps/plugin-dialog";
 import Swal from "sweetalert2";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "./components/ui/button";
+import { Progress } from "./components/ui/progress";
+import { Textarea } from "./components/ui/textarea";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage, FormDescription } from "./components/ui/form";
+import { Switch } from "./components/ui/switch";
+import { Dialog, DialogContent } from "./components/ui/dialog";
 
-type ElectionTemplate = {
-  name: string;
-  start: number;
-  end: number;
-  question: string;
-  choices: string;
-  signature_required: boolean;
-}
+const NU5 = 1687104
+
+const electionSchema = z.object({
+  name: z.string().min(3).max(120),
+  start: z.number().int().min(NU5),
+  end: z.number().int().min(NU5),
+  question: z.string().min(1),
+  choices: z.string().min(1),
+  signature_required: z.boolean(),
+})
+.refine((d) => d.end >= d.start,
+  { message: "End must be higher than Start", path: ["end"] });
 
 type ElectionData = {
   seed: string;
   election: any;
-}
+};
 
 function App() {
-  const [progress, setProgress] = useState<number | undefined>()
-  const [seed, setSeed] = useState<string>("")
-  const [election, setElection] = useState<any>()
-  const [showSeed, setShowSeed] = useState(false)
-  const { register, control, handleSubmit, formState: { errors } } = useForm<ElectionTemplate>({
+  const [progress, setProgress] = useState<number | undefined>();
+  const [seed, setSeed] = useState<string>("");
+  const [election, setElection] = useState<any>();
+  const [showSeed, setShowSeed] = useState(false);
+
+  const form = useForm<z.infer<typeof electionSchema>>({
+    resolver: zodResolver(electionSchema),
     defaultValues: {
       name: "",
-      start: 0,
-      end: 0,
+      start: NU5,
+      end: NU5,
       question: "",
       choices: "",
       signature_required: false,
-    }
-  })
-  const onSubmit: SubmitHandler<ElectionTemplate> = (data) => {
+    },
+  });
+
+  const { control, handleSubmit } = form;
+
+  const onSubmit: SubmitHandler<z.infer<typeof electionSchema>> = (data) => {
     (async () => {
-      const channel = new Channel<number>()
+      const channel = new Channel<number>();
       channel.onmessage = (p) => {
         setProgress(p);
-      }
+      };
 
       try {
-        const election: string = await invoke("create_election", { election: data, channel: channel, })
-        const electionData: ElectionData = JSON.parse(election)
-        const seed = electionData.seed
-        setSeed(seed)
-        setElection(electionData.election)
-        setShowSeed(true)
+        const election: string = await invoke("create_election", {
+          election: data,
+          channel: channel,
+        });
+        const electionData: ElectionData = JSON.parse(election);
+        const seed = electionData.seed;
+        setSeed(seed);
+        setElection(electionData.election);
+        setShowSeed(true);
+      } catch (e: any) {
+        await Swal.fire({
+          icon: "error",
+          title: e,
+        });
       }
-      catch (e: any) {
-        await Swal.fire(
-          {
-              icon: "error",
-              title: e
-          })
-      }
-
-    })()
-  }
+    })();
+  };
 
   const saveElectionFile = () => {
     (async () => {
-      setShowSeed(false)
+      setShowSeed(false);
       const path = await save({
         defaultPath: election.id,
-        title: 'Save Election File',
-        filters: [{
-          name: 'Election',
-          extensions: ['json']
-        }]
-      })
-      await invoke("save_election", { path: path, election: election })
-    })()
-  }
+        title: "Save Election File",
+        filters: [
+          {
+            name: "Election",
+            extensions: ["json"],
+          },
+        ],
+      });
+      await invoke("save_election", { path: path, election: election });
+    })();
+  };
 
   return (
     <main>
-      <Card className="max-w-full h-screen">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <h2 className="text-xl font-extrabold">Create an Election/Vote</h2>
-          <Label htmlFor="name">Name</Label>
-          <Controller control={control}
-            name="name"
-            render={({ field }) =>
-              <TextInput
-                {...field}
-                placeholder="Best Crypto"
-                color={errors.name && "failure"}
-                required
-                helperText={<span className="font-medium">{errors.name?.message}</span>}
-              />}
-          />
+      <Card className="w-3/4 m-auto p-2">
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="text-xl font-extrabold">Create an Election/Vote</h2>
+            <FormField
+              control={control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <div className="flex w-full gap-4">
-            <div className="flex flex-col">
-              <Label htmlFor="start">Registration Start</Label>
-              <Controller control={control}
-                name="start"
-                render={({ field }) =>
-                  <TextInput
-                    {...field}
-                    type="number"
-                    color={errors.start && "failure"}
-                    onChange={(v) => field.onChange(v.target.valueAsNumber)}
-                    required
-                    helperText={<span className="font-medium">{errors.start?.message}</span>}
-                  />}
-              />
-            </div>
+            <FormField
+              control={control}
+              name="start"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Registration Start</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} onChange={(v) => v && field.onChange(v.target.valueAsNumber)} />
+                  </FormControl>
+                  <FormDescription>Coins must be *younger* than that height to qualify</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-            <div className="flex flex-col">
-              <Label htmlFor="end">Start of Voting</Label>
-              <Controller control={control}
-                name="end"
-                render={({ field }) =>
-                  <TextInput
-                    {...field}
-                    type="number"
-                    color={errors.end && "failure"}
-                    onChange={(v) => field.onChange(v.target.valueAsNumber)}
-                    required
-                    helperText={<span className="font-medium">{errors.end?.message}</span>}
-                  />}
-              />
-            </div>
-          </div>
+            <FormField
+              control={control}
+              name="end"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Registration End</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} onChange={(v) => v && field.onChange(v.target.valueAsNumber)} />
+                  </FormControl>
+                  <FormDescription>Coins must be *older* than that height to qualify</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <Label htmlFor="question">Ballot Question</Label>
-          <Controller control={control}
-            name="question"
-            render={({ field }) =>
-              <TextInput
-                {...field}
-                color={errors.question && "failure"}
-                placeholder="What is the best cryptocurrency?"
-                required
-                helperText={<span className="font-medium">{errors.question?.message}</span>}
-              />}
-          />
+            <FormField
+              control={control}
+              name="question"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Ballot Question</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <Label htmlFor="choices">Ballot Choices</Label>
-          <Controller control={control}
-            name="choices"
-            render={({ field }) =>
-              <Textarea rows={10}
-                {...field}
-                color={errors.choices && "failure"}
-                placeholder="Use one line per choice"
-                required
-                helperText={<span className="font-medium">{errors.choices?.message}</span>}
-              />}
-          />
-          <div className="flex items-center gap-2">
-            <Checkbox id="signature_required" defaultChecked {...register("signature_required")} />
-            <Label htmlFor="signature_required" className="flex">
-              Signature Required
-            </Label>
-          </div>
-          <Button className="my-4" onClick={() => { }} type="submit">
-            Create Election File</Button>
-          {progress && <Progress progress={progress} />}
-        </form>
+            <FormField
+              control={control}
+              name="choices"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Ballot Choices</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} rows={10} />
+                  </FormControl>
+                  <FormDescription>Use one line per choice</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="signature_required"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex items-center justify-between">
+                  <FormLabel>Signature Required</FormLabel>
+                  <FormControl>
+                    <Switch checked={field.value} onCheckedChange={field.onChange} />
+                  </FormControl>
+                  </div>
+                  <FormDescription>If checked, voters must have the secret key</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="my-4" onClick={() => {}} type="submit">
+              Create Election File
+            </Button>
+            {progress && <Progress value={progress} />}
+          </form>
+        </Form>
       </Card>
-      <Modal show={showSeed}>
-        <Modal.Header>Election Seed</Modal.Header>
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-            <h3 className="mb-5 text-md font-normal text-gray-500 dark:text-gray-400">
-              You must save these 24 words in the correct order and spelling.
-              It is impossible to decode the votes without them.
-            </h3>
-            <h4 className="text-lg mb-4 border border-red-400">{seed}</h4>
-            <div className="flex justify-center gap-4">
-              <Button onClick={saveElectionFile}>
-                OK, I have saved them
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
+      <Dialog open={showSeed}>
+        <DialogContent>
+          <Card className="p-2">
+            <CardTitle>Election Seed</CardTitle>
+            <CardContent>
+              <h3 className="mb-5 text-md font-normal text-red-500 dark:text-gray-400 py-2">
+                You MUST save these 24 words in the correct order and spelling. It
+                is impossible to decode the votes without them.
+              </h3>
+              <h4 className="text-lg mb-4 border border-red-400 p-1">{seed}</h4>
+              <div className="flex justify-center gap-4">
+                <Button onClick={saveElectionFile}>OK, I have saved them</Button>
+              </div>
+            </CardContent>
+          </Card>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }
